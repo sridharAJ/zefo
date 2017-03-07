680e5d1ce7d352232358b2f01c5f2090c182d7d1
Sridhar Arumugam <sridhar.arumugam@myntra.com>
2017-03-07 16:08:15 +0530

    cleaned the server code

--- server.js
+++ server.js
@@ -3,30 +3,21 @@
  */
 const express = require('express');
 const morgan = require('morgan');
-const router = require('express').Router();
 const path = require('path');
 const proxy = require('express-http-proxy');
-var requestProxy = require('express-request-proxy');
 const app = express();
-const SuperAgent = require('superagent');
-const request = require('request')
+const request = require('request');
 
 // Setup logger
 app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
-
-// app.use('/proxy/:place/category/:item', function(req, res, next){
-//     console.log('https://www.gozefo.com/'+ req.params.place +'/category/'+ req.params.item + req.url)
-//
-//     SuperAgent.get('https://www.gozefo.com/'+ req.params.place +'/category/'+ req.params.item + req.url)
-//         .end((error, result) => {
-//             console.log(error,result.body)
-//             res.send(result.text);
-//         })
-// });
-
+app.use(express.static('build'));
 app.use('/proxy/:place/category/:item', function(req, res) {
     const url = 'https://www.gozefo.com/'+ req.params.place +'/category/'+ req.params.item + req.url;
     request.get(url).pipe(res);
+});
+
+app.use('*', function(req, res, next) {
+   res.sendFile(`${__dirname}/build/index.html`);
 });
 
 const PORT = process.env.PORT || 9000;
--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,13 @@
  * Created by 12072 on 06/03/17.
  */
 import React from 'react';
-import { Route } from 'react-router';
+import { Route, IndexRedirect } from 'react-router';
 import Category from './pages/Category';
 import App from './App';
 
 const AppRoutes = (
     <Route path='/' component={App}>
+        <IndexRedirect to='/bangalore/category/beds'/>
         <Route path={':city/category/:category'} component={Category}/>
     </Route>
 );
